93f946a2e6d51f2ad55b9ff85425e0d397e9d93d
jose.maycon <jose.maycon@LAB2-DCX-37.labs.dcx>
2020-02-13 07:46:40 -0300

    Passo quatro

---
+++ TrabEngenharia/src/Teste/MayconSouza.java
@@ -0,0 +1,15 @@
+package Teste;
+
+public class MayconSouza {
+	
+	public static void main(String[] args) {
+		
+		int numero1 = 20;
+		int numero2 = 40;
+		
+		int result = numero1 + numero2;
+		
+		System.out.println("Resultado da soma:" +result);
+	}
+
+}
